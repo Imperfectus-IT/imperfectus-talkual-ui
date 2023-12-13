4f89356adcf9823d9365cfde72cdba52f593d236
Luis Ramírez Calle <luis.ramirezcalle@outlook.com>
2023-12-13 14:12:53 +0000

    Fix button

--- src/components/primevue/TKButton/TKButton.d.ts
+++ src/components/primevue/TKButton/TKButton.d.ts
@@ -1,6 +1,6 @@
 import { ButtonHTMLAttributes } from 'vue';
 
-export interface ButtonProps extends /* @vue-ignore */ ButtonHTMLAttributes {
+export interface TKButtonProps extends /* @vue-ignore */ ButtonHTMLAttributes {
     style?: any;
     class?: any;
     label?: string | undefined;
@@ -23,4 +23,33 @@ export interface ButtonProps extends /* @vue-ignore */ ButtonHTMLAttributes {
     pt?: any;
     ptOptions?: any;
     unstyled?: boolean;
+}
+/**
+ * Defines valid slots in Button component.
+ */
+export interface TKButtonSlots {
+    /**
+     * Custom content such as icons, images and text can be placed inside the button via the default slot. Note that when slot is used, label, icon and badge properties are not included.
+     */
+    default(): VNode[];
+    /**
+     * Custom icon template.
+     * @param {Object} scope - icon slot's params.
+     */
+    icon(scope: {
+        /**
+         * Style class of the icon.
+         */
+        class: string;
+    }): VNode[];
+    /**
+     * Custom loading icon template.
+     * @param {Object} scope - loading icon slot's params.
+     */
+    loadingicon(scope: {
+        /**
+         * Style class of the loading icon.
+         */
+        class: string;
+    }): VNode[];
 }
--- src/components/primevue/TKButton/TKButton.vue
+++ src/components/primevue/TKButton/TKButton.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import Button from "primevue/button"
-import { ButtonProps } from "./TKButton";
+import { TKButtonProps, TKButtonSlots } from "./TKButton";
 
-const props = withDefaults(defineProps<ButtonProps>(), {
+const props = withDefaults(defineProps<TKButtonProps>(), {
   iconPos: 'left',
   loading: false,
   link: false,
@@ -12,8 +12,17 @@ const props = withDefaults(defineProps<ButtonProps>(), {
   outlined: false,
   plain: false
 })
+const slots = defineSlots<TKButtonSlots>()
 </script>
 
 <template>
-  <Button v-bind="props"/>
+  <Button v-bind="props">
+    <slot v-if="slots.default" name="default" />
+    <template v-if="slots.icon" v-slot:icon="scope">
+      <slot name="icon" v-bind="scope" />
+    </template>
+    <template v-if="slots.loadingicon" v-slot:loadingicon="scope">
+      <slot name="loadingicon" v-bind="scope" />
+    </template>
+  </Button>
 </template>
